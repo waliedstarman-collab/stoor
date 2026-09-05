28bb24e4c3bb000d7373397f3aa8294480122114
waliedstarman-collab <waliedstar.man@gmail.com>
2026-09-05 12:24:22 +0000

    Fix unclosed brace in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-         if (app()->environment('production')) {
-        \Illuminate\Support\Facades\URL::forceScheme('https');
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
