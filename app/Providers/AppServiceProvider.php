0163efaa3f3affe58ae8a2bb5722d1b88d40b5b3
waliedstarman-collab <waliedstar.man@gmail.com>
2026-09-05 12:13:25 +0000

    Force HTTPS for all URLs in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+         if (app()->environment('production')) {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 }
