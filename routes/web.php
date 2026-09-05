a289d53e4b4495851ab058a044dabb7d8267f88a
waliedstarman-collab <waliedstar.man@gmail.com>
2026-09-05 13:04:06 +0000

    Add test-auth route for debugging

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,10 @@ use App\Http\Controllers\FrontendController;
 Route::get('/', [FrontendController::class, 'index'])->name('website.home');
 Route::get('/category/{slug}', [FrontendController::class, 'category'])->name('website.category');
 Route::get('/product/{id}', [FrontendController::class, 'product'])->name('website.product');
+
+Route::get('/test-auth', function () {
+    if (auth()->check()) {
+        return '✅ User is logged in: ' . auth()->user()->email;
+    }
+    return '❌ User is NOT logged in';
+})->middleware('web');
